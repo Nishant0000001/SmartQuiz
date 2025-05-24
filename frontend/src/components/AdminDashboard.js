1ece7765c165b0d507507cf958ce08e88ec64476
Nishant Bijalwan <121458337+Nishant0000001@users.noreply.github.com>
2025-05-25 00:42:17 +0530

    Update AdminDashboard.js

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -19,7 +19,7 @@ function AdminPage() {
   const [message, setMessage] = useState('');
   const [activeTab, setActiveTab] = useState('add');
 
-  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+  const backendUrl = 'https://smartquiz-t8un.onrender.com' || 'http://localhost:3000';
 
   useEffect(() => {
     fetchQuestions();
